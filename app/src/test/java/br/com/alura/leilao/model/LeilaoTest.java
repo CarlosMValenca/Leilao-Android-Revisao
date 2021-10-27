2e6ce2cdc84b84195e366aac0e806cd7fcd02924
Carlos M Valenca <cvalenca@everis.com>
2021-10-27 16:34:41 -0300

    Criando testes de unidade

--- app/src/test/java/br/com/alura/leilao/model/LeilaoTest.java
+++ app/src/test/java/br/com/alura/leilao/model/LeilaoTest.java
@@ -7,19 +7,41 @@ import org.junit.Test;
 public class LeilaoTest {
 
     @Test
-    public void getDescricao() {
+    public void getDescricaoDevolvendoDescricao() {
         Leilao console = new Leilao("Console");
         String descricaoDevolvida = console.getDescricao();
         assertEquals("Console", descricaoDevolvida);
     }
 
     @Test
-    public void getMaiorLance(){
+    public void getMaiorLanceQuandoDadoMaiorLance(){
         Leilao console = new Leilao("Console");
         console.propoe(new Lance(new Usuario("Alex"), 200.0));
 
         double maiorLanceDevolvido = console.getMaiorLance();
 
         assertEquals(200.0, maiorLanceDevolvido, 0.0001);
+    }
+
+    @Test
+    public void getMaiorLanceEntreDoisLancesOrdemCrescente(){
+        Leilao computador = new Leilao("Computador");
+        computador.propoe(new Lance(new Usuario("Alex"), 100.0));
+        computador.propoe(new Lance(new Usuario("Fran"), 200.0));
+
+        double maiorLanceDevolvidoComputador = computador.getMaiorLance();
+
+        assertEquals(200.0, maiorLanceDevolvidoComputador, 0.0001);
+    }
+
+    @Test
+    public void getMaiorLanceEntreDoisLancesOrdemDescrescente(){
+        Leilao carro = new Leilao("Carro");
+        carro.propoe(new Lance(new Usuario("Alex"), 10000.0));
+        carro.propoe(new Lance(new Usuario("Fran"), 9000.0));
+
+        double maiorLanceDevolvidoCarro = carro.getMaiorLance();
+
+        assertEquals(10000.0, maiorLanceDevolvidoCarro, 0.0001);
     }
 }
