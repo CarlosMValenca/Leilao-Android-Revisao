c68dd8e2228600fb370c2fcfecea29c5ecac6707
Carlos M Valenca <cvalenca@everis.com>
2021-10-28 13:54:31 -0300

    Quatro lances

--- app/src/test/java/br/com/alura/leilao/model/LeilaoTest.java
+++ app/src/test/java/br/com/alura/leilao/model/LeilaoTest.java
@@ -119,4 +119,18 @@ public class LeilaoTest {
         assertEquals(300.0, tresMaioresLancesDevolvidos.get(0).getValor(), DELTA);
         assertEquals(200.0, tresMaioresLancesDevolvidos.get(1).getValor(), DELTA);
     }
+
+    @Test
+    public void deve_DevolverTresMaioresLances_QuandoReceberApenasQuatroLances(){
+        CONSOLE.propoe(new Lance(ALEX, 300.0));
+        CONSOLE.propoe(new Lance(FRAN, 400.0));
+        CONSOLE.propoe(new Lance(ALEX, 500.0));
+        CONSOLE.propoe(new Lance(FRAN, 600.0));
+
+        List<Lance> tresMaioresLancesDevolvidos = CONSOLE.tresMaioresLances();
+        assertEquals(3, tresMaioresLancesDevolvidos.size());
+        assertEquals(600.0, tresMaioresLancesDevolvidos.get(0).getValor(), DELTA);
+        assertEquals(500.0, tresMaioresLancesDevolvidos.get(1).getValor(), DELTA);
+        assertEquals(400.0, tresMaioresLancesDevolvidos.get(2).getValor(), DELTA);
+    }
 }
