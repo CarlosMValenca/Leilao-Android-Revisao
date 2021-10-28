16831471a85e76ce8f9af8a9d61a3fb9c65461bd
Carlos M Valenca <cvalenca@everis.com>
2021-10-28 10:17:16 -0300

    Aplicando TDD

--- app/src/test/java/br/com/alura/leilao/model/LeilaoTest.java
+++ app/src/test/java/br/com/alura/leilao/model/LeilaoTest.java
@@ -4,73 +4,86 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import java.util.List;
+
 public class LeilaoTest {
 
-    private Leilao console = new Leilao("Console");
-    private Usuario alex = new Usuario("Alex");
-    private Usuario fran = new Usuario("Fran");
+    private final Leilao CONSOLE = new Leilao("Console");
+    private final Usuario ALEX = new Usuario("Alex");
+    private final Usuario FRAN = new Usuario("Fran");
 
     @Test
     public void deve_DevolverDescricao_QuandoREcebeDescricao() {
-        String descricaoDevolvida = console.getDescricao();
+        String descricaoDevolvida = CONSOLE.getDescricao();
         assertEquals("Console", descricaoDevolvida);
     }
 
     @Test
     public void deve_DevolverMaiorLance_QuandoRecebeMaiorLance(){
-        console.propoe(new Lance(alex, 200.0));
+        CONSOLE.propoe(new Lance(ALEX, 200.0));
 
-        double maiorLanceDevolvido = console.getMaiorLance();
+        double maiorLanceDevolvido = CONSOLE.getMaiorLance();
 
         assertEquals(200.0, maiorLanceDevolvido, 0.0001);
     }
 
     @Test
     public void deve_DevolverMaiorLance_QuandoRecebeEntreDoisLancesOrdemCrescente(){
-        console.propoe(new Lance(alex, 100.0));
-        console.propoe(new Lance(fran, 200.0));
+        CONSOLE.propoe(new Lance(ALEX, 100.0));
+        CONSOLE.propoe(new Lance(FRAN, 200.0));
 
-        double maiorLanceDevolvido = console.getMaiorLance();
+        double maiorLanceDevolvido = CONSOLE.getMaiorLance();
 
         assertEquals(200.0, maiorLanceDevolvido, 0.0001);
     }
 
     @Test
     public void deve_DevolverMaiorLance_QuandoRecebeEntreDoisLancesOrdemDescrescente(){
-        console.propoe(new Lance(alex, 10000.0));
-        console.propoe(new Lance(fran, 9000.0));
+        CONSOLE.propoe(new Lance(ALEX, 10000.0));
+        CONSOLE.propoe(new Lance(FRAN, 9000.0));
 
-        double maiorLanceDevolvido = console.getMaiorLance();
+        double maiorLanceDevolvido = CONSOLE.getMaiorLance();
 
         assertEquals(10000.0, maiorLanceDevolvido, 0.0001);
     }
 
     @Test
     public void deve_DevolverMenorLance_QuandoRecebeMenorLance(){
-        console.propoe(new Lance(alex, 200.0));
+        CONSOLE.propoe(new Lance(ALEX, 200.0));
 
-        double menorLanceDevolvido = console.getMenorLance();
+        double menorLanceDevolvido = CONSOLE.getMenorLance();
 
         assertEquals(200.0, menorLanceDevolvido, 0.0001);
     }
 
     @Test
     public void deve_DevolverMenorLance_QuandoRecebeEntreDoisLancesOrdemCrescente(){
-        console.propoe(new Lance(alex, 100.0));
-        console.propoe(new Lance(fran, 200.0));
+        CONSOLE.propoe(new Lance(ALEX, 100.0));
+        CONSOLE.propoe(new Lance(FRAN, 200.0));
 
-        double menorLanceDevolvido = console.getMenorLance();
+        double menorLanceDevolvido = CONSOLE.getMenorLance();
 
         assertEquals(100.0, menorLanceDevolvido, 0.0001);
     }
 
     @Test
     public void deve_DevolverMenorLance_QuandoRecebeEntreDoisLancesOrdemDescrescente(){
-        console.propoe(new Lance(alex, 10000.0));
-        console.propoe(new Lance(fran, 9000.0));
+        CONSOLE.propoe(new Lance(ALEX, 10000.0));
+        CONSOLE.propoe(new Lance(FRAN, 9000.0));
 
-        double menorLanceDevolvido = console.getMenorLance();
+        double menorLanceDevolvido = CONSOLE.getMenorLance();
 
         assertEquals(9000.0, menorLanceDevolvido, 0.0001);
+    }
+
+    @Test
+    public void deve_DevolverTresMaioresLances_QuandoRecebeExatosTresLances(){
+        CONSOLE.propoe(new Lance(ALEX, 100.0));
+        CONSOLE.propoe(new Lance(FRAN, 200.0));
+        CONSOLE.propoe(new Lance(ALEX, 300.0));
+
+        List<Lance> tresMaioresLancesDevolvidos = CONSOLE.tresMaioresLances();
+
+        assertEquals(3, tresMaioresLancesDevolvidos.size());
     }
 }
